dba9f607a2c40ceec67bde26bbb6d0b5127e6529
takata yuta <takata@benest.co.jp>
2022-11-11 10:03:46 +0900

    second commit

---
+++ pages/postcss.config.js
@@ -0,0 +1,11 @@
+// postcss.config.js
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
